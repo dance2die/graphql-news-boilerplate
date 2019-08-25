1b7244c715b3435ec475b1eae98b713443c36a41
Sung M. Kim <sungmkim80@gmail.com>
2019-08-24 23:22:40 -0400

    Added two queries, Link & Links

--- server/index.js
+++ server/index.js
@@ -3,13 +3,26 @@ import graphqlHTTP from 'express-graphql'
 import { buildSchema } from 'graphql'
 
 const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
+  type Link {
+      _id: Int!
+      url: String!
+      description: String!
+  }
+
+  type Query {
+      links: [Link!]!
+      link(_id: Int!): Link!
+  }
 `)
 
+const links = [
+  { _id: 0, url: 'https://example.com', description: 'example site' },
+  { _id: 1, url: 'https://google.com', description: 'Google site' },
+  { _id: 2, url: 'https://Microsoft.com', description: 'Microsoft site' },
+]
 const rootValue = {
-  hello: () => 'Hello GraphQL world!',
+  links: () => links,
+  link: ({ _id }) => links.filter(link => link._id === _id)[0],
 }
 
 const app = express()
